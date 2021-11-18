4234b5b0b5090d9ab1ce6c62cc057e6d18ca7fd3
ricardombrodriguez <ricardorodriguez@ua.pt>
2021-11-18 17:09:00 +0000

    authentication (done)

--- Projeto/app/models.py
+++ Projeto/app/models.py
@@ -2,24 +2,13 @@ from django.db import models
 
 # Create your models here.
 #base de dados inicial se virmos que queremos mais campos é so dar atualizar
+
+
 class Groups(models.Model):
-    #id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=70)
 
     def __str__(self):
         return self.description
-
-
-class Users(models.Model):
-    #ver uma função para encriptar a pass
-    first_name = models.CharField(max_length=70)
-    last_name = models.CharField(max_length=70)
-    password = models.CharField(max_length=50)
-    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
-
-
-    def __str__(self):
-        return self.first_name +self.last_name
 
 
 class Publication_status(models.Model):
@@ -38,28 +27,36 @@ class Publication_topics(models.Model):
         return self.description
 
 
+class Users(models.Model):
+    username= models.CharField(max_length=70,default="")
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
+    favourites = models.ManyToManyField('Publications',blank=True)
+
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
 class Publications(models.Model):
     title = models.CharField(max_length=70)
-    content = models.CharField(max_length=200)
-    creted_on = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=1000)
+    created_on = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Users, on_delete=models.CASCADE)
-    status=models.ForeignKey(Publication_status, on_delete=models.CASCADE)
+    status = models.ForeignKey(Publication_status, on_delete=models.CASCADE)
     topic = models.ForeignKey(Publication_topics, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
+
+
 class Comments(models.Model):
-    comment = models.CharField(max_length=200)
+    comment = models.CharField(max_length=300)
     author = models.ForeignKey(Users, on_delete=models.CASCADE)
     publication = models.ForeignKey(Publications, on_delete=models.CASCADE)
     def __str__(self):
         return self.comment
-
-
-class Favorites(models.Model):
-    author = models.ForeignKey(Users, on_delete=models.CASCADE)
-    publication = models.ForeignKey(Publications, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.author + self.publication
 
 
--- Projeto/app/views.py
+++ Projeto/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterUser
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+
+def register_request(request):
+    if request.method == "POST":
+        form = RegisterUser(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("login.html")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = RegisterUser()
+    return render(request=request, template_name="register.html", context={"register_form" : form})
