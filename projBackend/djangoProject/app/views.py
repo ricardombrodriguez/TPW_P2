7eca0c43502bd963f6b962cb75af0715ee71836f
alexandre <alexandreserras@ua.pt>
2022-01-12 19:43:56 +0000

    Api os métodos essenciais ja esta, precisam apenas de certos updates e validações

--- projBackend/djangoProject/app/views.py
+++ projBackend/djangoProject/app/views.py
@@ -3,8 +3,9 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from app.models import Author, Groups,Users
-from app.serializers import AuthorSerializer, GroupsSerializer,UsersSerializer
+from app.models import Author, Groups, Users, Publication_status, Publication_topics, Publications, Comments, Favorites
+from app.serializers import AuthorSerializer, GroupsSerializer, UsersSerializer, PublicationStatusSerializer, \
+    PublicationTopicsSerializer, PublicationsSerializer, CommentsSerializer, FavoritesSerializer
 
 
 # Create your views here.
@@ -109,4 +110,217 @@ def get_user(request):
     serializer = UsersSerializer(user)
     return Response(serializer.data)
 
+# Apagar users'?????
+
 ##Publication Status
+#Nao vai ter create nem delete so vai ter gets
+
+
+@api_view(['GET'])
+def pub_status_getALl(request):
+
+    ret = Publication_status.objects.all()
+    serializer = PublicationStatusSerializer(ret, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def pub_status_getOne(request):
+    #Acho que é melhor fazer o get Pela descrição do que pelo id
+    id = int(request.GET['id'])
+    try:
+        ret = Publication_status.objects.get(id=id)
+    except Publication_status.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = PublicationStatusSerializer(status)
+    return Response(serializer.data)
+
+
+## Publication Topics
+@api_view(['GET'])
+def get_pub_topic(request):
+    #Penso que faça mais sentido fazer o get pela descrição do que pelo id
+    id = int(request.GET['id'])
+    try:
+        ret = Publication_topics.objects.get(id=id)
+    except Publication_topics.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = PublicationTopicsSerializer(ret)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_pub_topics(request):
+    ret= Publication_topics.objects.all()
+    serializer = PublicationTopicsSerializer(ret,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def get_pub_topics_create(request):
+    serializer = PublicationTopicsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def get_pub_topics_update(request):
+    # Não sei se vai ser pelo id se pela descrição, vou deixar pelo id e mais tarde troca-se se for preciso
+    id = request.data['id']
+    try:
+        ret=Publication_topics.objects.get(id=id)
+    except Publication_topics.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer=PublicationTopicsSerializer(ret,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def get_pub_topics_delete(request,id):
+    try:
+        ret=Publication_topics.objects.get(id=id)
+    except Publication_topics.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    ret.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+##PUBLICATION
+@api_view(['GET'])
+def pub(request):
+    id = int(request.GET['id'])
+    try:
+        ret = Publications.objects.get(id=id)
+    except Publications.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = PublicationsSerializer(ret)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def pubs(request):
+    ret= Publications.objects.all()
+    serializer = PublicationsSerializer(ret,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def pubcrate(request):
+    serializer = PublicationsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def pubupd(request):
+    id = request.data['id']
+    try:
+        ret=Publications.objects.get(id=id)
+    except Publications.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer=PublicationsSerializer(ret,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def pubdel(request,id):
+    try:
+        ret=Publications.objects.get(id=id)
+    except Publications.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    ret.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+##COMMENTS
+    #NOS NO OUTRO PROJETO NÃO TINHAMOS O UPDATE DE UM COMENTÁRIO não sei se querem colocar ou não, se quiserem
+    # depois meto
+@api_view(['GET'])
+def comment(request):
+    id = int(request.GET['id'])
+    try:
+        ret = Comments.objects.get(id=id)
+    except Comments.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = CommentsSerializer(ret)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def comments(request):
+    ret= Comments.objects.all()
+    serializer = CommentsSerializer(ret,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def commentcre(request):
+    serializer = CommentsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['DELETE'])
+def commentdel(request,id):
+    try:
+        ret=Comments.objects.get(id=id)
+    except Comments.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    ret.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+## FAVORITOS
+# Aqui não faz sentido ter updates, porque ou é favorito ou não é
+
+@api_view(['GET'])
+def fav(request):
+    id = int(request.GET['id'])
+    try:
+        ret = Favorites.objects.get(id=id)
+    except Favorites.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = FavoritesSerializer(ret)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def favs(request):
+    ret= Favorites.objects.all()
+    serializer = FavoritesSerializer(ret,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def favcre(request):
+    serializer = FavoritesSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['DELETE'])
+def favdel(request,id):
+    try:
+        ret=Favorites.objects.get(id=id)
+    except Favorites.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    ret.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
