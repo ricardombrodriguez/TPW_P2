212b82e8d4797fa534475aafda8fd85c61804a67
alexandre <alexandreserras@ua.pt>
2022-01-18 22:38:06 +0000

    Endpoints devem estar quase todos SIGAAA FAZER ANGULAR

--- projBackend/djangoProject/app/models.py
+++ projBackend/djangoProject/app/models.py
@@ -2,12 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class  Author(models.Model):
-    name =models.CharField(max_length=100)
-    email= models.CharField(max_length=100)
 
-    def __str__(self):
-        return str(self.name)
 
 
 class Groups(models.Model):
